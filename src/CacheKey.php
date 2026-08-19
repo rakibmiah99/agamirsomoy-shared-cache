69c404c78dff48ece7b1d6184988e8b9561ccf5d
Md. Rakib Miah <69368862+rakibmiah99@users.noreply.github.com>
2026-08-19 17:48:42 +0600

    Rename namespace and update composer configuration for shared cache library

--- src/CacheKey.php
+++ src/CacheKey.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Rakib\SharedCache;
+namespace Rakibmiah99\AgamirsomoySharedCache;
 
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Config;
--- src/SharedCacheServiceProvider.php
+++ src/SharedCacheServiceProvider.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Rakib\SharedCache;
+namespace Rakibmiah99\AgamirsomoySharedCache;
 
 use Illuminate\Support\ServiceProvider;
 
